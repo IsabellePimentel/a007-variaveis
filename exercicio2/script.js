cb3cc173f96d9e2749569f05a3327397d9ff649d
isabellepimentel <isabellechavespimentel@gmail.com>
2022-08-29 21:36:51 -0300

    Exercicio A007

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+const perguntaA = "Você almoçou hoje?";
+const respostaA = prompt(perguntaA);
+
+const perguntaB = "Você lanchou hoje?";
+const respostaB = prompt(perguntaB);
+
+const perguntaC = "Você jantou hoje?";
+const respostaC= prompt(perguntaC);
+
+console.log(`${perguntaA},${respostaA}`)
+console.log(`${perguntaB},${respostaB}`)
+console.log(`${perguntaC},${respostaC}`)
---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+let a = 10
+let b = 25
+
+
+
+let valortemporario = b
+b = a
+a = valortemporario
+
+console.log("O novo valor de a é", a) 
+console.log("O novo valor de b é", b) 
